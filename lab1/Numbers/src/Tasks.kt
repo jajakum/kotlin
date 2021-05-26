c10abb933e28d381e529902e9ec8e37f55cfb606
jajakum <jajakumm@gmail.com>
2021-05-26 15:10:41 +0300

    task 7
    added task 7

--- lab1/Numbers/src/Tasks.kt
+++ lab1/Numbers/src/Tasks.kt
@@ -1,6 +1,17 @@
+import java.lang.Math.abs
+
 fun main() {
     val number = inputNumber()
-    println("Sum of digits of number: ${sumDigits(number)}")
+    println()
+
+    // task 6
+    // println("Sum of digits of number: ${sumDigits(number)}")
+
+    // task 7
+    println("Sum of digits: ${sumDigits(number)}")
+    println("Max digit: ${maxDigit(number)}")
+    println("Min digit: ${minDigit(number)}")
+    println("Mult of digits: ${multDigits(number)}")
 }
 
 // ввод числа (рекурсия, пока число некорректно)
@@ -19,9 +30,56 @@ fun inputNumber(): Int {
         }
 }
 
-// сумма цифр числа
+// task 6: сумма цифр числа
 fun sumDigits(number: Int): Int =
     if (number / 10 == 0)
         number % 10
     else
         sumDigits(number / 10) + (number % 10)
+
+// task 7.1: максимальная цифра числа
+fun maxDigit(number: Int): Int {
+    fun maxDigit(number: Int, max: Int): Int {
+        val newNumber = number / 10
+        val digit = abs(number % 10)
+
+        val newMax =
+            if (digit > max)
+                digit
+            else
+                max
+
+        return if (newNumber != 0)
+            maxDigit(newNumber, newMax)
+        else
+            newMax
+    }
+    return maxDigit(number, -1)
+}
+
+// task 7.2: минимальная цифра числа
+fun minDigit(number: Int): Int {
+    fun minDigit(number: Int, min: Int): Int {
+        val newNumber = number / 10
+        val digit = abs(number % 10)
+
+        val newMin =
+            if (digit < min)
+                digit
+            else
+                min
+
+        return if (newNumber != 0)
+            minDigit(newNumber, newMin)
+        else
+            newMin
+    }
+    return minDigit(number, 10)
+}
+
+// task 7.2: проиведение цифр числа
+fun multDigits(number: Int): Int =
+    if (number / 10 == 0)
+        abs(number % 10)
+    else
+        multDigits(number / 10) * abs(number % 10)
