6ab35b2a068edfff81ceb0fa15d2cdee620488ef
jajakum <jajakumm@gmail.com>
2021-05-26 14:49:20 +0300

    added task 13.3

--- lab1/helloworld/src/Tasks.kt
+++ lab1/helloworld/src/Tasks.kt
@@ -1,8 +1,21 @@
 fun main (args: Array<String>)
 {
-    task1()
+    task3()
 }
 
 /* Установить Intellij Idea, JDK и плагин для Kotlin
    Реализовать Hello World */
 fun task1() = println("Hello, World!")
+
+/* Принять имя пользователя как аргумент программы
+Поздороваться с пользователем. Применить форматирование строки */
+fun task3() {
+    val name = inputName()
+    println("Hello, $name!")
+}
+
+// ввод имени пользователя
+fun inputName(): String? {
+    print("Enter the user name: ")
+    return readLine()
+}
